5b3238f6d150235a22f9679eac59c9ee12f13698
CarterTheGreat <cdwlions2016@gmail.com>
2019-01-12 11:18:38 -0500

    Door control first test

---
+++ DoorOpener.ino
@@ -0,0 +1,94 @@
+#include <Wire.h>
+#include <Servo.h>
+#include <Adafruit_MotorShield.h>
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+Adafruit_DCMotor *motor = AFMS.getMotor(1);
+Servo servo;
+
+void setup() {
+  Serial.begin(9600);  
+
+  AFMS.begin();  //1.6KHz
+ 
+  //speed from 0 to 255
+  motor->setSpeed(0);
+  motor->run(RELEASE);
+  
+  servo.attach(9);
+  servo.write(map(0,0,255,0,180));
+  delay(2000);
+  
+}
+
+void loop() {
+
+
+  Serial.println("Start");
+  raiseMotor();
+  delay(2000);
+  lowerMotor();
+  delay(5000);
+
+  openDoor();
+  delay(2000);
+
+  closeDoor();
+  delay(2000);
+  Serial.println("End");
+
+}
+
+
+void lowerMotor(){
+
+  
+  Serial.println("Lower");
+  
+  for (int i=0; i<=35; i++) {
+    servo.write(map(i, 0, 255, 0, 180));
+    delay(1);
+  }
+}
+
+void raiseMotor(){
+  
+  Serial.println("Raise");
+  
+  for (int i=35; i>=0; i--) {
+    servo.write(map(i, 0, 255, 0, 180));
+    delay(1);
+  }
+}
+
+void openDoor(){
+  uint8_t i;
+  Serial.println("Open");
+  
+  motor->run(FORWARD);
+  motor->setSpeed(i); 
+  for (i=0; i<255; i++) {
+    motor->setSpeed(i);  
+    delay(10);
+  }
+  delay(1000);
+  
+  
+  motor->run(RELEASE);
+  
+}
+
+void closeDoor(){
+  uint8_t i;
+  Serial.println("Close");
+
+  motor->run(BACKWARD);
+  for (i=0; i<255; i++) {
+    motor->setSpeed(i);  
+    delay(10);
+  }
+  delay(1000);
+  
+  motor->run(RELEASE);
+}
